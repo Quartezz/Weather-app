a1ff58ba9699f4ca8deb10a10369d37e4a67d36b
Quartezz <kamil.machel03@gmail.com>
2023-09-09 16:09:55 +0200

    added function for getting api

--- src/api.js
+++ src/api.js
@@ -1,10 +1,15 @@
 async function getData(city) {
     const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=5384bb69bc2a4cf2878214847230809`
     try {
-
+        const response = await fetch(api, { mode: "cors"})
+        const data = convertData(await response.json())
+        return data
     } catch (error) {
         alert (error)
         return;
     }
-
+    
 }
+
+    return { getData };
+
