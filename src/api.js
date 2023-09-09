9c590097fb91c2e56d1eda8427e1fde906a56fd0
Quartezz <kamil.machel03@gmail.com>
2023-09-09 13:14:50 +0200

    api skeleton code

---
+++ src/api.js
@@ -0,0 +1,10 @@
+async function getData(city) {
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=5384bb69bc2a4cf2878214847230809`
+    try {
+
+    } catch (error) {
+        alert (error)
+        return;
+    }
+
+}
